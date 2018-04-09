fb07b5c05958d82d8bb67e7e1140027fd32af637
Rbetances <rbetances@vimenca.com>
2018-04-09 15:44:04 -0400

    Se elimino el componente de articulo y se unio al home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ArticleComponent } from './article/article.component';
 import { ArticlesService } from './articles.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ArticleComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
